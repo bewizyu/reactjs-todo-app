5df7527b31216776aad8f31f80f959127fa80bce
Nicolas Hodicq <nartawak@gmail.com>
2019-02-14 11:49:56 +0100

    React Context architecture

--- src/todos/TodoForm.js
+++ src/todos/TodoForm.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import {TextField, FlatButton} from 'material-ui';
+import {FlatButton, TextField} from 'material-ui';
 import AddIcon from 'material-ui/svg-icons/content/add';
 import DeleteIcon from 'material-ui/svg-icons/action/delete';
-import PropTypes from 'prop-types';
+import TodoContext from './TodoContext';
 
 export class TodoForm extends React.Component {
-  
   constructor(props) {
     super(props);
-    
     this.state = {
       value: '',
     };
-    
     this.handleAddClick = this.handleAddClick.bind(this);
-    this.handleRemoveClick = this.handleRemoveClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
   
@@ -24,41 +20,40 @@ export class TodoForm extends React.Component {
     });
   };
   
-  handleAddClick () {
+  handleAddClick(addTodo) {
     if (this.state.value) {
-      this.props.addTodo(this.state.value);
+      addTodo(this.state.value);
       this.setState(() => ({
         value: '',
       }));
     }
   }
   
-  handleRemoveClick () {
-    this.props.removeTodo();
-  }
-  
-  render () {
+  render() {
     return (
-      <div>
-        <TextField
-          value={this.state.value}
-          floatingLabelText="Saisir un todo"
-          onChange={this.handleChange}
-        />
-        <FlatButton
-          icon={<AddIcon />}
-          onClick={this.handleAddClick}
-        />
-        <FlatButton
-          icon={<DeleteIcon />}
-          onClick={this.handleRemoveClick}
-        />
-      </div>
+      <TodoContext.Consumer>
+        {
+          ({addTodo, removeTodos}) => {
+            return (
+              <div>
+                <TextField
+                  value={this.state.value}
+                  floatingLabelText="Saisir un todo"
+                  onChange={this.handleChange}
+                />
+                <FlatButton
+                  icon={<AddIcon />}
+                  onClick={() => this.handleAddClick(addTodo)}
+                />
+                <FlatButton
+                  icon={<DeleteIcon />}
+                  onClick={() => removeTodos()}
+                />
+              </div>
+            )
+          }
+        }
+      </TodoContext.Consumer>
     );
   }
-}
-
-TodoForm.propTypes = {
-  addTodo : PropTypes.func.isRequired,
-  removeTodo : PropTypes.func.isRequired,
 }
--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,40 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import {List, ListItem, Checkbox} from 'material-ui';
+import {Checkbox, List, ListItem} from 'material-ui';
+import TodoContext from './TodoContext';
 
-export class TodoList extends React.Component {
-  
-  updateTodoState (todo) {
-    this.props.updateTodo(todo);
-  }
-  
-  render () {
-    if (this.props.todos.length === 0) {
-      return <div>Vous n'avez pas de todo</div>
-    }
-    
-    return (
-      <List>
-        {this.props.todos.map((todo) => {
-          return <ListItem key={todo.id}
-                           primaryText={todo.title}
-                           style={todo.isDone ? {textDecoration: 'line-through'} : undefined}
-                           leftCheckbox={<Checkbox checked={todo.isDone}
-                                                   onCheck={this.updateTodoState.bind(this, todo)}/>}/>
-        })
+export function TodoList() {
+  return (
+    <TodoContext.Consumer>
+      {
+        ({todos, updateTodo}) => {
+          if (todos.length === 0) {
+            return <div>Vous n'avez pas de todo</div>
+          }
+          return (
+            <List>
+              {todos.map((todo) => {
+                return <ListItem key={todo.id}
+                                 primaryText={todo.title}
+                                 style={todo.isDone ? {textDecoration: 'line-through'} : undefined}
+                                 leftCheckbox={<Checkbox checked={todo.isDone}
+                                                         onCheck={() => updateTodo(todo)} />} />
+              })
+              }
+            </List>
+          );
         }
-      </List>
-    );
-  }
-}
-
-TodoList.propTypes = {
-  todos : PropTypes.arrayOf(
-    PropTypes.shape({
-      id : PropTypes.number.isRequired,
-      title : PropTypes.string.isRequired,
-      isDone: PropTypes.bool.isRequired,
-    })
-  ).isRequired,
-  updateTodo : PropTypes.func.isRequired,
+      }
+    </TodoContext.Consumer>
+  );
 }
